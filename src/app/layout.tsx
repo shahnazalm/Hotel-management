a237c10c764832f9f4afa93314d640207319b8ca
shahnaz.mhethawy <shahnaz.mhethawy@student.uib.no>
2024-05-07 23:32:41 +0200

    Added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className="font-normal">{children}</main>
+        <main className="font-normal">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-betweem">
+      <div className="flex items-center w-full md:2/3">
+        <Link href={"/"} className="font-black text-[#F27405] ">
+          Hotelzz
+        </Link>
+        <ul className="flex item-center ml-5">
+          <li className="flex items-center">
+            <Link href={"/auth"}>
+              <FaUserCircle className="cursor-pointer" />
+            </Link>
+          </li>
+          <li className="ml-2">
+            <MdDarkMode className="cursor_pointer" />
+          </li>
+        </ul>
+      </div>
+
+      <ul className=" flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href={"/room"}>Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href={"/"}>Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+export default Header;
