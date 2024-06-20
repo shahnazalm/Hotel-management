77d6c495ba09df0b379bb23216678c62bffc1c8c
Shahnaz Almhethawy <shahnazalmhethawy@Anass-MacBook-Pro.local>
2024-06-20 23:15:56 +0200

    Add all schemas to index.ts file

---
+++ schemaTypes/index.ts
@@ -0,0 +1,15 @@
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
+import user from "./user";
+import verficationToken from "./verficationToken";
+
+export const schemaTypes = [
+  user,
+  account,
+  booking,
+  hotelRoom,
+  review,
+  verficationToken,
+];
